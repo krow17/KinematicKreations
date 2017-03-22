6c05a9b0ba3270e8586c1537feae6fc3d12aa38c
cjf2222 <cjf2222@tamu.edu>
2017-03-22 11:51:27 -0500

    Add files via upload

---
+++ L8Toy/Assets/Scripts/LShape.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Color : MonoBehaviour 
+{
+	enum color {Blue,Green,Yellow,Red};
+}
+
+public class LShape : MonoBehaviour {
+
+	int layer;
+	Color color;
+
+	void Start () {
+		transform.position = new Vector3 (0, 0, 0);
+		AddPiece();
+		AddPiece();
+		CheckLocation();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void AddPiece(){
+		Config.configuration.Add(this.gameObject);
+		Debug.Log (Config.configuration.Count);
+	}
+
+	Vector3 CheckLocation(){
+		//Vector3 vec = new Vector3 (0, 0, 0);
+		//vec = Config.configuration [0].transform.position;
+		//return vec;
+		return this.transform.position;
+	}
+
+
+}
