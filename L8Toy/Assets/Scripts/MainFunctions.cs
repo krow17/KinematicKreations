bd4c26f215bdca4ad082d02ca0612cf2463f0604
cjf2222 <cjf2222@tamu.edu>
2017-04-16 19:10:30 -0500

    Add files via upload

--- L8Toy/Assets/Scripts/MainFunctions.cs
+++ L8Toy/Assets/Scripts/MainFunctions.cs
@@ -41,17 +41,18 @@ public class MainFunctions : MonoBehaviour {
 			Debug.Log ("Limit of pices reached");
 		}
 
+
 		//instantiate piece and add it to the correct files
 	}
 
 	public void removePiece(GameObject pieceToRemove)
 	{
 		GameManager.instance.config.configuration.Remove(pieceToRemove);
-		// REMOVE ALL JOINTS!!!!!!!!
-		//List<Magnet> magnets = pieceToRemove.GetComponentsInChildren<Magnet>();
-		//foreach(Magnet m in magnets)
+
+		Magnet[] magnets = pieceToRemove.GetComponentsInChildren<Magnet>();
+		foreach(Magnet m in magnets)
 		{
-			//removeJoint (m);
+			//removeJoint (m); //pass in game object
 		}
 		Destroy (pieceToRemove);
 	}
@@ -77,17 +78,43 @@ public class MainFunctions : MonoBehaviour {
 
 	public void addJoint(GameObject magnetOne, GameObject magnetTwo)
 	{
-		//check they are free or ERROR
-		//check Poles are opposite or ERROR
-		// make magnet.connection = to opposite for each
-		//connect joints
-		//create joint between pieces
-	}
+		if (magnetOne.GetComponent<Magnet>().connection == null && magnetTwo.GetComponent<Magnet>().connection == null) {
+			if (magnetOne.GetComponent<Magnet>().pole != magnetTwo.GetComponent<Magnet>().pole)
+			{
+				magnetOne.GetComponent<Magnet>().connection = magnetTwo;
+				magnetTwo.GetComponent<Magnet>().connection = magnetOne;
 
+				magnetOne.AddComponent<HingeJoint>();
+				magnetOne.GetComponent<HingeJoint>().axis = new Vector3(0,10,0);
+
+				magnetTwo.AddComponent<HingeJoint>();
+				magnetTwo.GetComponent<HingeJoint>().axis = new Vector3(0,10,0);
+
+
+				magnetOne.GetComponent<HingeJoint> ().connectedBody = magnetTwo.GetComponent<Rigidbody> ();
+				magnetTwo.GetComponent<HingeJoint> ().connectedBody = magnetOne.GetComponent<Rigidbody> ();
+
+				//magnetOne.GetComponentInParent<HingeJoint> ().connectedBody = magnetTwo.GetComponentInParent<Rigidbody> ();
+				//magnetTwo.GetComponentInParent<HingeJoint> ().connectedBody = magnetOne.GetComponentInParent<Rigidbody> ();
+
+			}
+		}
+
+
+	}
+	 
 	public void removeJoint(GameObject magnet)
 	{
-		if (magnet.GetComponent<Magnet> ().connection != null) {
-			//remove joint between pieces
+		if (magnet.GetComponent<Magnet>().connection != null) {
+			
+			GameObject obj = magnet.GetComponent<Magnet>().connection;
+
+			Destroy(magnet.GetComponent<HingeJoint>());
+			Destroy(obj.GetComponent<HingeJoint>());
+
+			obj.GetComponent<Magnet>().connection = null;
+			magnet.GetComponent<Magnet>().connection = null;
+
 		}
 	}
 
@@ -101,6 +128,29 @@ public class MainFunctions : MonoBehaviour {
 		{
 			Camera.main.transform.Translate (Vector3.back * Time.deltaTime * GameManager.instance.parameters.zoomSpeed);
 		}
+	}
+
+	public void changeColorOfSelectedL(string color)
+	{
+		if (GameManager.instance.config.currentSelection != null) {
+			switch(color)
+			{
+			case "blue":
+				GameManager.instance.config.currentSelection.GetComponent<LShape> ().changeColor (LShape.Colors.Blue);
+				break;
+			case "yellow":
+				GameManager.instance.config.currentSelection.GetComponent<LShape> ().changeColor (LShape.Colors.Yellow);
+				break;
+			case "white":
+				GameManager.instance.config.currentSelection.GetComponent<LShape> ().changeColor (LShape.Colors.White);
+				break;
+			case "red":
+				GameManager.instance.config.currentSelection.GetComponent<LShape> ().changeColor (LShape.Colors.Red);
+				break;
+
+			}
+		}
+
 	}
 
 
